fe5ec1810611a2558272660c20d2f0f791744f8e
j-blalock <justin.m.blalock@gmail.com>
2020-11-30 17:54:05 -0600

    setting dev dependencies

---
+++ src/Words.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Words() {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Final"),
+    React.createElement("h2", {}, "Project"),
+    React.createElement("h2", {}, "React"),
+  ]);
+}
